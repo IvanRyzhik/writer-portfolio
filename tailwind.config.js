ebb9a2ebda4ee3b3b2ad783a7466a20f6b518ff8
IvanRyzhik <rimoli18@gmail.com>
2025-08-29 14:35:58 +0200

    settings update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './source/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
     './node_modules/flowbite/**/*.js',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
