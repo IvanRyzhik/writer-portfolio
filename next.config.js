c493234a097520b1cac49e3725aaade210ef6d56
IvanRyzhik <rimoli18@gmail.com>
2025-08-29 12:10:57 +0200

    rules update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ['source.unsplash.com'],
   },
-}
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      (warn) =>
+        warn.module?.resource?.includes('flowbite.min.css') &&
+        warn.message?.includes('autoprefixer'),
 
-module.exports = nextConfig
+      (warn) =>
+        warn.message?.includes('Skipped not serializable cache item') &&
+        warn.message?.includes('flowbite.min.css'),
+    ];
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./source/**/*.{js,ts,jsx,tsx}",    // якщо є папка source з кодом
-    "./app/**/*.{js,ts,jsx,tsx}",       // якщо з часом додаси app router
-    "./node_modules/flowbite/**/*.js"
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './source/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
+  plugins: [require('flowbite/plugin')],
+};
