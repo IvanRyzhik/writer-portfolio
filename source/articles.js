9e9881656176e7fb82b9bfbfd382820c641ab3df
IvanRyzhik <rimoli18@gmail.com>
2025-09-22 18:26:14 +0200

    add  new articles

--- source/articles.js
+++ source/articles.js
@@ -3,7 +3,6 @@ export const articles = [
     id: 3,
     src: '/images/preview-3.png',
     title: 'Headway vs. Blinkist Comparison: Which Summary App Suits Your Learning Style',
-    alt: 'Headway vs. Blinkist Comparison: Which Summary App Suits Your Learning Style',
     preview:
       'Sometimes, I pick up a book I bought, read a few pages, and realize it’s not quite what I expected. Then I suddenly remember why it’s been doing such a great job as a coaster under my mug (at least it’s supporting something)',
     source: 'https://www.hustleinspireshustle.com/blog/headway-vs-blinkist-comparison',
@@ -13,7 +12,6 @@ export const articles = [
     id: 4,
     src: '/images/preview-4.png',
     title: 'The Morning Routines of Tech CEOs: Habits Backed by Science',
-    alt: 'The Morning Routines of Tech CEOs: Habits Backed by Science',
     preview:
       'We can’t seem to get enough of CEOs morning routines and their stories. From early wake-ups to silent meditation, these details fill LinkedIn posts and podcasts. But what is it about someone else’s day that keeps us so curious? Part of the fascination comes from hope: the idea that small morning choices might spark bigger changes like increased discipline or even similar success.',
     source:
@@ -24,7 +22,6 @@ export const articles = [
     id: 5,
     src: '/images/preview-5.png',
     title: 'How Upskilling Transforms Entry-Level Employees Into Leaders',
-    alt: 'How Upskilling Transforms Entry-Level Employees Into Leaders',
     preview:
       'Being a leader isn’t just about creating strategy decks or writing performance reviews. It often shows up in quieter moments like supporting a teammate or taking responsibility. And that’s exactly where leadership upskilling makes a difference: it helps employees build the mindset, soft skills and confidence to step into these roles.',
     source: 'https://yulys.com/blog/how-upskilling-transforms-entry-level-employees-into-leaders',
@@ -34,7 +31,6 @@ export const articles = [
     id: 6,
     src: '/images/preview-6.png',
     title: 'Leveraging Microlearning Apps for Enhanced Employee Productivity',
-    alt: 'Leveraging Microlearning Apps for Enhanced Employee Productivity',
     preview:
       'If your employees enjoy quizzes, rewards or interactive challenges during corporate training, microlearning could be perfect for them. It involves short, focused learning sessions that might include a set of lessons with reading cards, achievements or just statistics with the progress metrics. Some microlearning apps and platforms provide stats with comparison to other users.',
     source:
@@ -44,12 +40,59 @@ export const articles = [
   {
     id: 2,
     src: '/images/preview-2.png',
-    alt: 'Microlearning Strategies for Reskilling the Modern Workforce: A Practical Guide',
     title: 'Microlearning Strategies for Reskilling the Modern Workforce: A Practical Guide',
     preview:
       'As automation accelerates, reskilling is essential. Microlearning—short, targeted lessons—offers just-in-time knowledge that fits busy schedules and evolving job needs. From scenario-based modules to gamification and peer insights, these strategies enable efficient, relevant learning and foster adaptability, ensuring employees remain competitive in a rapidly changing job market.',
     source:
       'https://www.thehrdirector.com/microlearning-strategies-reskilling-modern-workforce-practical-guide/',
+    remoteAccess: true,
+  },
+  {
+    id: 7,
+    src: '/images/preview-7.png',
+    title: 'Best Audiobooks for Road Trips: Family-Friendly Picks',
+    preview:
+      'Before you hit the road, double-check you’ve got snacks, water, and something good queued up to listen to. And if you’ve ever spent hours searching for the best audiobooks for road trips or wondered about the easiest way to access content on the go, we’ve found some of the most amazing books and tools to check out.',
+    source:
+      'https://hnmagazine.co.uk/features/best-audiobooks-for-road-trips-family-friendly-picks/',
+    remoteAccess: true,
+  },
+  {
+    id: 8,
+    src: '/images/preview-8.png',
+    title: 'Top Areas of Personal Growth for Career Success in 2025',
+    preview:
+      'In 2025, professionals are paying attention to key areas of personal growth that really impact their careers. They’re learning new tools, improving project management for hybrid teams, updating certifications, and setting work-life boundaries to prevent burnout. This shift also comes as the tech sector faces significant changes, including the global layoffs driven by economic uncertainty, and rapid adoption of AI.',
+    source:
+      'https://www.theindustryleaders.org/post/top-areas-of-personal-growth-for-career-success-in-2025',
+    remoteAccess: true,
+  },
+  {
+    id: 9,
+    src: '/images/preview-9.png',
+    title: 'Books to Inspire Travel: Your next destination awaits',
+    preview:
+      'Today, we’re sharing top books that feature travelling. Whether you enjoy nonfiction journeys or real stories about adventures across countries, this list covers a unique variety of books to inspire travel and is based on user feedback and bestseller rankings.',
+    source: 'https://canarianweekly.com/posts/Books-to-Inspire-Travel-Your-next-destination-awaits',
+    remoteAccess: true,
+  },
+  {
+    id: 10,
+    src: '/images/preview-10.png',
+    title: 'Benefits of Microlearning vs. Traditional Training Methods',
+    preview:
+      'Ever felt like you can’t even sit through a single sitcom episode anymore because you get bored and you get this urgent need to check your phone? Turns out, dopamine is partly to blame, but luckily, you can use the benefits of microlearning to take control of your attention.',
+    source:
+      'https://www.sciencetimes.com/articles/60559/20250825/benefits-microlearning-vs-traditional-training-methods.htm',
+    remoteAccess: false,
+  },
+  {
+    id: 11,
+    src: '/images/preview-11.png',
+    title: 'Benefits of Microlearning vs. Traditional Training Methods',
+    preview:
+      'Ever felt like you can’t even sit through a single sitcom episode anymore because you get bored and you get this urgent need to check your phone? Turns out, dopamine is partly to blame, but luckily, you can use the benefits of microlearning to take control of your attention.',
+    source: 'https://millo.co/best-self-improvement-guide-to-help-you-achieve-work-life-balance',
     remoteAccess: true,
   },
   // {
